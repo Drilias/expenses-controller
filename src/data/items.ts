0fd169eac30b8dc03de09c40e3e22a0fb525d7db
Adriana Ferreira <78874092+Drilias@users.noreply.github.com>
2021-10-09 23:54:05 -0300

    Add files via upload

---
+++ src/data/items.ts
@@ -0,0 +1,6 @@
+export const items = [
+    {date:new Date(2021,9,8), category: 'food', title:'McDonalds',value:32.15},
+    {date:new Date(2021,9,8), category: 'food', title:'Burguer King',value:28.99},
+    {date:new Date(2021,9,7), category: 'rent', title:'Aluguel Apt',value:2300},
+    {date:new Date(2021,10,15), category: 'salary', title:'Salário',value:6000},
+];
